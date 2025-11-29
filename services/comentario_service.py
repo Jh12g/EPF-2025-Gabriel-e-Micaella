640a86ace2c08483a9bdd694076f221a2ab01ea3
micaella <micaellalps03@gmail.com>
2025-11-29 14:41:41 -0300

    corrigindo a classe comenrio_service.py

--- services/comentario_service.py
+++ services/comentario_service.py
@@ -11,7 +11,7 @@ class ComentarioService:
         """
         try:
             # gerador de ID  
-            todos = self.comentarioModel.retornarTudo()
+            todos = self.model.listar_todos()
             novo_id = 1
             if todos:
                 novo_id = max(c['id'] for c in todos) + 1
@@ -20,13 +20,13 @@ class ComentarioService:
             novo_comentario = EntidadeComentario(
                 id=novo_id,
                 texto=texto,
-                autor=autor,
-                receita=receita
+                autor_id=autor,
+                receita_id=receita
             )
 
             # Acessa a lista interna da BaseAbstrata (_dados) e salva
-            self.comentarioModel._dados.append(novo_comentario.tradutorJ())
-            self.comentarioModel.salvar() # Persiste no disco
+            self.model.dados.append(novo_comentario.to_dict())
+            self.model.salvar() # Persiste no disco
             return True
         except Exception as e:
             print(f"Erro ao criar comentário: {e}")
@@ -34,22 +34,21 @@ class ComentarioService:
 
     # CONFERIR MELHOR DEPOIS!!!!
 
-    def listarPorReceita(self, receita_alvo):
-        """
-        Filtra os comentários para mostrar apenas os de uma receita específica.
-        """
-        todos = self.comentarioModel.retornarTudo()
-        # List Comprehension: cria uma nova lista apenas com os itens que passam no teste (if)
-        return [c for c in todos if c['receita'] == receita_alvo]
-
+    def listar_por_receita(self, receita_id):
+        todos = self.model.listar_todos()
+        return [c for c in todos if c['receita_id'] == receita_id]
+    
     def editar(self, id_comentario, novo_texto):
         """
         Busca um comentário pelo ID e atualiza seu texto.
         """
-        comentario_dict = self.comentarioModel.buscarID(id_comentario)
-        if comentario_dict:
-            comentario_dict['texto'] = novo_texto
-            self.comentarioModel.salvar() # Salva a alteração no arquivo
+        comentario = self.model.buscar_por_id(id_comentario)
+        if comentario:
+            # gabriel, como em python dicionários são passados por referência, mudar aqui muda direto na lista self.model.dados
+            comentario['texto'] = novo_texto
+            
+            # 3. Manda salvar a lista atualizada no arquivo
+            self.model.salvar()
             return True
         return False
 
@@ -57,9 +56,12 @@ class ComentarioService:
         """
         Remove um comentário da lista e atualiza o arquivo.
         """
-        comentario_dict = self.comentarioModel.buscarID(id_comentario)
-        if comentario_dict:
-            self.comentarioModel._dados.remove(comentario_dict)
-            self.comentarioModel.salvar()
+        comentario = self.model.buscar_por_id(id_comentario)
+        
+        if comentario:
+            # remove o item da lista
+            self.model.dados.remove(comentario)
+            # salva a lista sem o item
+            self.model.salvar()
             return True
         return False
