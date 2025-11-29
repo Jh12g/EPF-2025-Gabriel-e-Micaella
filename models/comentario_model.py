0298723cf15cf310368fe3c8ff2aac0a2d835f8f
micaella <micaellalps03@gmail.com>
2025-11-29 14:08:55 -0300

    explicando p gabriel sobre a classe comentario_model

--- models/comentario_model.py
+++ models/comentario_model.py
@@ -6,10 +6,12 @@ class ComentarioModel(BaseModel):
         # chamando o construtor da classe pai (seguindo o modelo das outras classes)
         super().__init__('comentarios.json')
 
+#base model ja salva lista e carrega ent nao precisa reescrever elas + buscar por id
+
     # buscar o id de quem comentou
-    def buscar_por_id(self, id_item):
+    def buscar_por_id(self, id_item): 
         return next((c for c in self.dados if c['id'] == id_item), None)
     
-
-    # não tenho certeza de como é pra ser feito o tradutorJ 
-    # (micaella se vc souber melhor essa parte da um help)
+# gabriel aqui n precisa de to_dict pq ja tem na entidadeComentario
+#lembrando q as entidades servem p segurar os dados e essas models unicas serve pra gravar e ler no disco
+# essas entidades sao como folhas de papeis preenchidas com os dados e as model fazem o trabalho d gerenciar (gurdar e recuperar) as fichas (((service cordena tudo como regras calculos logica)))
