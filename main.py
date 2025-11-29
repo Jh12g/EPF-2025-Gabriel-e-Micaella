6014f50fa6ada3f32930e393a78e5f022b69005a
micaella <micaellalps03@gmail.com>
2025-11-29 16:28:19 -0300

    arrumando main.py mas tem que ver direito depois

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from app import create_app
+from app import App
 
 if __name__ == '__main__':
-    app = create_app()
-    app.run()
+    print("Iniciando Servidor de Receitas...")
+    
+    #cria a aplicação
+    minha_app = App().get_app()
+    
+    # roda o servidor
+    # debug=True e reloader=True ajudam muito pq mostram erros na tela mas dps apago quando finalizar a fase teste
+    minha_app.run(host='localhost', port=8080, debug=True, reloader=True)
